c4960bc8041f7d2c74d874eaa6f5b76038aabda3
Ryan Harris <harris.rf@gmail.com>
2017-03-03 07:01:35 +0200

    Update space_management.sh
    Included du command

--- space_management.sh
+++ space_management.sh
@@ -8,3 +8,6 @@ tar -cvzf archive_name.tar.gz *file_name_pattern*.*
 
 # Move files
 mv /mount_point1/archive_name.tar.gz /mount_point2/
+
+# Check for largest files and folders
+du -sch /mount_point/folder/* | sort -rh
